e4378ab9964b01ea4d19e3e665dfbadbd0352c29
Victor Mwendwa <vmwendwa704@gmail.com>
2026-05-01 19:12:34 +0300

    Done with this project

--- script.js
+++ script.js
@@ -5,7 +5,18 @@ const weatherInfoSection = document.querySelector(".weather-info");
 const notFoundSection = document.querySelector(".not-found");
 const searchCitySection = document.querySelector(".search-city");
 
-const API_KEY = "";
+const countryTxt = document.querySelector(".country-txt");
+const tempTxt = document.querySelector(".temp-txt");
+const ConditionTxt = document.querySelector(".condition-txt");
+const humidityValueTxt = document.querySelector(".humidity-value-txt");
+const windValueTxt = document.querySelector(".wind-value-txt");
+const weatherSummaryImage = document.querySelector(".weather-summary-img");
+const currentDateTxt = document.querySelector(".current-date-txt");
+
+const forecastItemContainer = document.querySelector(".forecast-items-container");
+
+import { API_KEY } from "./config.js";
+
 
 searchButton.addEventListener("click", () => {
   if (cityInput.value.trim() != "") {
@@ -23,12 +34,34 @@ cityInput.addEventListener("keydown", (event) => {
   }
 });
 
+
 async function getFetchData(endPoint, city) {
   const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${API_KEY}&units=metric`;
 
   const response = await fetch(apiUrl);
 
   return response.json();
+}
+
+function getWeatherIcon(id) {
+  if (id <= 232) return "thunderstorm.svg";
+  if (id <= 321) return "drizzle.svg";
+  if (id <= 531) return "rain.svg";
+  if (id <= 622) return "snow.svg";
+  if (id <= 781) return "atmosphere.svg";
+  if (id <= 800) return "clear.svg";
+  else return "clouds.svg";
+}
+
+function getCurrentDate() {
+  const currentDate = new Date()
+  const options = {
+    weekday: 'short',
+    day: '2-digit',
+    month: 'short'
+  }
+
+  return currentDate.toLocaleDateString('en-GB', options);
 }
 
 async function updateWeatherInfo(city) {
@@ -38,17 +71,71 @@ async function updateWeatherInfo(city) {
     showDisplaySection(notFoundSection);
     return;
   }
-console.log(weatherData);
+  console.log(weatherData);
+  const {
+    name: country,
+    main: { temp, humidity },
+    weather: [{ id, main }],
+    wind: { speed },
+  } = weatherData;
 
-const {
-     name:country, 
-    main:{temp, humidity}, 
-    weather:[{id, main:weatherCondition, description:weatherDescription}],
-        wind:speed
- } = weatherData;
+  countryTxt.textContent = country;
+  tempTxt.textContent = `${Math.round(temp)}°C`;
+  ConditionTxt.textContent = main;
+  humidityValueTxt.textContent = `${humidity}%`;
+  windValueTxt.textContent = `${speed} M/s`;
 
+    currentDateTxt.textContent = getCurrentDate();
+  weatherSummaryImage.src = `assets/weather/${getWeatherIcon(id)}`;
+
+   await updateForecastsInfo(city);
   showDisplaySection(weatherInfoSection);
 }
+
+async function updateForecastsInfo(city) {
+  const forecastsData = await getFetchData('forecast', city);
+
+  const timeTaken = '12:00:00';
+  const todayDate = new Date().toISOString().split('T')[0];
+
+  forecastItemContainer.innerHTML = '';
+  forecastsData.list.forEach(forecastWeather => {
+    if(forecastWeather.dt_txt.includes(timeTaken) && !forecastWeather.dt_txt.includes(todayDate)) {
+      upateForecastItems(forecastWeather)
+    }
+  });
+}
+
+function upateForecastItems(weatherData) {
+console.log(weatherData);
+const {
+    dt_txt:date,
+    main:{temp},
+    weather:[{id}]    
+} = weatherData
+
+const dateTaken = new Date(date)
+const dateOption = {
+    day:'2-digit',
+    month:'short'
+}
+
+const dateResult = dateTaken.toLocaleDateString('en-US', dateOption)
+const forecastItem = `
+<div class="forecast-item">
+<h5 class="forecast-item-date regular-txt">${dateResult}</h5>
+<img src="assets/weather/${getWeatherIcon(id)}" alt="weather icon" class="forecast-item-img">
+    <h5 class="forecast-item-temp">${Math.round(temp)}°C</h5 >
+</div>
+`;
+
+forecastItemContainer.insertAdjacentHTML('beforeend', forecastItem);
+}
+
+
+
+
+
 
 function showDisplaySection(section) {
   [weatherInfoSection, searchCitySection, notFoundSection].forEach(
